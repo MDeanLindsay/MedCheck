fb6e1527a3b3fba9a0b1e1119edc06b3eaff782e
Michael Lindsay <michaeldeanlindsay@gmail.com>
2019-09-10 00:38:48 -0700

    added packages

--- Program.cs
+++ Program.cs
@@ -1,26 +1,20 @@
 ﻿using System;
-using NUnit.Framework;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Support.UI;
-using OpenQA.Selenium.Chrome;
-using System.Collections.Generic;
 using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 class Program
 {
     static void Main()
     {
-        using (IWebDriver driver = new ChromeDriver("C:\\"))
+        using (IWebDriver driver = new ChromeDriver("."))
         {
             driver.Navigate().GoToUrl("https://www.caremc.com/caremcwebmvc/account/login");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
             driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
             string parentWindowHandle = driver.CurrentWindowHandle;
             
-            // Still not necessarily sure why I "have" to reassign links twice to get this to run.
-
             var links = driver.FindElements(By.XPath("/html/body/div[2]/header/div[1]/div/div/a"));
-
             for (int i = 0; i < links.Count; i++)   {
                 links = driver.FindElements(By.XPath("/html/body/div[2]/header/div[1]/div/div/a"));
                 links[i].Click();
@@ -35,15 +29,11 @@ class Program
             }
             
             var links2 = driver.FindElements(By.XPath("/html/body/div[2]/header/div[2]/div/ul/li/a"));
-
             for (int i = 0; i < links2.Count; i++)  {
                 links2 = driver.FindElements(By.XPath("/html/body/div[2]/header/div[2]/div/ul/li/a"));
                 links2[i].Click();
                 driver.Navigate().Back();
             }
-
-            // Thrown off iterating by button/input.
-            // Is there an easier to go about looping through without ids, similar tags, etc?
            
             driver.FindElement(By.XPath("//button[@class=\"SubmitBlue\"]")).Click();
             driver.Navigate().Back();
@@ -53,7 +43,7 @@ class Program
             driver.FindElement(By.XPath("//input[@id=\"UserName\"]")).Click();
             driver.FindElement(By.XPath("//input[@id=\"UserName\"]")).SendKeys("test");
 
-            Console.WriteLine("It worked!!!");
+            Console.WriteLine("It compiled?");
             driver.Quit();
         }    
     }
